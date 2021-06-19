fcdaaf3052a17e9becef9598c86aee89e3c9a9b4
Obagunwa Emmanuel <access2emma@gmail.com>
2021-06-19 14:16:25 +0100

    added lottie files

---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Driplar
+//
+//  Created by Emmanuel O on 19/06/2021.
+//
+
+import Foundation
--- src/screens/MainApplication/Settings/Help.js
+++ src/screens/MainApplication/Settings/Help.js
@@ -1,15 +1,58 @@
 import React, { useState } from "react";
-import { View, StyleSheet, TouchableOpacity, ScrollView, TextInput as AppTextInput } from "react-native";
+import { TextInput } from "react-native-paper";
+import { View, StyleSheet, TouchableOpacity, ScrollView, TextInput as AppTextInput, Dimensions } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-import { TextInput } from "react-native-paper";
+import LottieView from "lottie-react-native";
+
+import Animated, {
+    runOnJS,
+    withTiming,
+    interpolate,
+    useSharedValue,
+    useAnimatedStyle,
+    interpolateColor,
+} from "react-native-reanimated";
 
 import theme from "../../../theme";
 import { AppText, TwoColumnTab, Button } from "../../../components";
 
+const { height } = Dimensions.get("window");
+
 export const Help = ({ navigation }) => {
+    const modalAnimation = useSharedValue(0);
+    const [showModal, setShowModal] = useState(false);
     const [currentTab, setCurrentTab] = useState("FAQs");
+
+    const openModal = () => {
+        setShowModal(true);
+        modalAnimation.value = withTiming(1, { duration: 500 });
+    };
+
+    const closeModal = () => {
+        modalAnimation.value = withTiming(0, { duration: 500 }, () => {
+            "worklet";
+            runOnJS(setShowModal)(false);
+        });
+    };
+
+    const modalContentStyle = useAnimatedStyle(() => {
+        return {
+            opacity: modalAnimation.value,
+            transform: [{ translateY: interpolate(modalAnimation.value, [0, 1], [height, 0]) }],
+        };
+    }, []);
+
+    const overlayStyle = useAnimatedStyle(() => {
+        return {
+            backgroundColor: interpolateColor(
+                modalAnimation.value,
+                [0, 1],
+                ["rgba(255, 255, 255, 0)", "rgba(0, 0, 0, 0.8)"],
+            ),
+        };
+    }, []);
 
     const renderContent = () => {
         if (currentTab === "FAQs") {
@@ -49,33 +92,35 @@ export const Help = ({ navigation }) => {
             );
         } else {
             return (
-                <View style={styles.form}>
-                    <TextInput
-                        label="First name"
-                        style={styles.formGroup}
-                        underlineColor="transparent"
-                        theme={{ colors: { background: "#fff" } }}
-                    />
-                    <TextInput
-                        label="Email"
-                        style={styles.formGroup}
-                        underlineColor="transparent"
-                        theme={{ colors: { background: "#fff" } }}
-                    />
+                <>
+                    <View style={styles.form}>
+                        <TextInput
+                            label="First name"
+                            style={styles.formGroup}
+                            underlineColor="transparent"
+                            theme={{ colors: { background: "#fff" } }}
+                        />
+                        <TextInput
+                            label="Email"
+                            style={styles.formGroup}
+                            underlineColor="transparent"
+                            theme={{ colors: { background: "#fff" } }}
+                        />
 
-                    <AppTextInput
-                        multiline={true}
-                        textAlignVertical="top"
-                        style={styles.formTextarea}
-                        placeholder="Send us your questions, feedback or suggestions."
-                    />
+                        <AppTextInput
+                            multiline={true}
+                            textAlignVertical="top"
+                            style={styles.formTextarea}
+                            placeholder="Send us your questions, feedback or suggestions."
+                        />
 
-                    <View style={styles.addImageBox}>
-                        <Icon name="plus" size={20} color={theme.colors.label} />
+                        <View style={styles.addImageBox}>
+                            <Icon name="plus" size={20} color={theme.colors.label} />
+                        </View>
+
+                        <Button label="Submit" style={styles.formBtn} onPress={openModal} />
                     </View>
-
-                    <Button label="Submit" style={styles.formBtn} />
-                </View>
+                </>
             );
         }
     };
@@ -103,6 +148,37 @@ export const Help = ({ navigation }) => {
                     {renderContent()}
                 </View>
             </ScrollView>
+            {showModal && (
+                <>
+                    <Animated.View
+                        onPress={closeModal}
+                        onTouchEnd={closeModal}
+                        style={[StyleSheet.absoluteFill, overlayStyle]}
+                    />
+
+                    <Animated.View pointerEvents="box-none" style={[styles.overlay]}>
+                        <Animated.View style={[styles.overlayContent, modalContentStyle]}>
+                            <View style={styles.handle} />
+
+                            <LottieView
+                                autoPlay
+                                style={{ width: 100 }}
+                                source={require("../../../../assets/lottie/success.json")}
+                            />
+
+                            <AppText variant="medium" style={styles.topContentTitle}>
+                                Message sent!
+                            </AppText>
+                            <AppText style={styles.topContentDescription}>
+                                Thank you for contacting us. We’ll get back to you really soon.
+                            </AppText>
+                            <View style={styles.buttonBox}>
+                                <Button label="Okay" style={styles.cancelBtn} onPress={closeModal} />
+                            </View>
+                        </Animated.View>
+                    </Animated.View>
+                </>
+            )}
         </SafeAreaView>
     );
 };
@@ -161,5 +237,41 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
         backgroundColor: "#F0F0F0",
+    },
+    overlay: {
+        ...StyleSheet.absoluteFill,
+        justifyContent: "flex-end",
+    },
+    overlayContent: {
+        alignItems: "center",
+        backgroundColor: theme.colors.white,
+        borderTopLeftRadius: theme.radius.md,
+        borderTopRightRadius: theme.radius.md,
+    },
+    handle: {
+        width: 50,
+        height: 4,
+        marginTop: 10,
+        marginBottom: 30,
+        borderRadius: 20,
+        backgroundColor: "#C7C7CC",
+    },
+    topContentTitle: {
+        fontSize: 22,
+    },
+    topContentDescription: {
+        marginTop: 10,
+        lineHeight: 20,
+        width: "70%",
+        textAlign: "center",
+        color: theme.colors.label,
+    },
+    buttonBox: {
+        width: "90%",
+        marginTop: 20,
+        marginVertical: 30,
+    },
+    cancelBtn: {
+        marginTop: 8,
     },
 });
